0e54c0161902d657549cba104bf65f9779e7fd8a
DACASINMIKKO <mikkoangelodacasin@gmail.com>
2023-09-01 22:24:45 +0800

    filter side bar commits

--- components/filter-sidebar/FilterSidebar.jsx
+++ components/filter-sidebar/FilterSidebar.jsx
@@ -11,10 +11,10 @@ const FilterSidebar = () => {
     { name: "Jeans" },
   ];
   return (
-    <div className="p-5 w-1/4 bg-red-200 flex flex-col justify-between">
+    <div className="mb-[20px] p-5 w-1/5 bg-white flex flex-col justify-between border-2 border-[#0000001A] rounded-lg">
       <div>
-        <div className="mb-3">Filters</div>
-        <div className="flex flex-col py-3 border-y-2 border-gray-400 gap-2">
+        <div className="mb-[24px] font-bold text-[20px]">Filters</div>
+        <div className="flex flex-col py-[24px] border-y-2 border-[#0000001A] gap-2">
           {filtersItem.map((item) => (
             <div
               className="flex justify-between align-middle cursor-pointer"
@@ -31,8 +31,22 @@ const FilterSidebar = () => {
           ))}
         </div>
       </div>
-      <div>Price</div>
-      <div>Colors</div>
+      <div className="border-b-2 border-[#0000001A] py-[24px] flex flex-col justify-center">
+        <div className="mb-[24px] font-bold text-[20px]">Price</div>
+        <div>
+          <div>Min:</div>
+          <input
+            type="number"
+            className="border-2 border-[#0000001A] rounded-md outline-none"
+          />
+          <div>Max:</div>
+          <input
+            type="number"
+            className="border-2 border-[#0000001A] rounded-md outline-none"
+          />
+        </div>
+      </div>
+      <div className="border-b-2 border-[#0000001A] py-[24px]">Colors</div>
       <div>Style</div>
       <div>Dress Style</div>
     </div>
